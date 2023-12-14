455e81a5d0298a97612107d9f143133a66ee0081
DarioSierra00 <dario.sierramartinez@gmail.com>
2023-12-14 14:20:25 +0100

    subiendo

---
+++ src/main/java/com/jacaranda/employeeCompany/repository/CompanyRepository.java
@@ -0,0 +1,12 @@
+package com.jacaranda.employeeCompany.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jacaranda.employeeCompany.model.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
+	
+}
